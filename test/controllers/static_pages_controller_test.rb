5b79d7408a7a78852f6942068a25b93b30522b56
Derek Hobbs <dhobbs@madisoncollege.edu>
2017-03-10 03:41:20 +0000

    Got passing tests, now we need to add passwords

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,32 +2,32 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
   end
 
   test "should get generateWorkout" do
-    get static_pages_generateWorkout_url
+    get generate_workout_path
     assert_response :success
   end
 
   test "should get displayWorkout" do
-    get static_pages_displayWorkout_url
+    get display_workout_path
     assert_response :success
   end
 
   test "should get generateMeal" do
-    get static_pages_generateMeal_url
+    get generate_meal_path
     assert_response :success
   end
 
   test "should get userTracking" do
-    get static_pages_userTracking_url
+    get user_tracking_path
     assert_response :success
   end
 
