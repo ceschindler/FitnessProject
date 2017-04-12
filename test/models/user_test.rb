37a5180c707c16993662c356cd27f6c5b4a00fcf
Collin Schinder <cschindler@madisoncollege.edu>
2017-04-12 16:08:52 +0000

    fixed formatting issues and naming issues in User test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,40 +3,32 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    # See my other comment about arguments. I know this is direct from the book
-    # but as I have said in class he is formtting things to fit them on a page
-    # not always in the way I have laid out as correct in the course styleguide.
-    @user = User.new(name: "Joe User", email: "joe@example.com",
-                    password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Joe User",
+                     email: "joe@example.com",
+                     password: "foobar", 
+                     password_confirmation: "foobar")
   end
 
   test "should be valid" do
     assert @user.valid?
   end
   
-  # Here wouldn't a better name for the test be 'an empty string is not valid'?
-  test "name should be present" do
+  test "name should not be an empty string" do
     @user.name = "     "
     assert_not @user.valid?
   end
   
-  # Same as above
-  test "email should be present" do
+  test "email should not be an empty string" do
     @user.email = "     "
     assert_not @user.valid?
   end
   
   test "email validation should accept valid addresses" do
-    # more alignment issues. with word arrays I'm even ok with them being longer
-    # than 80 characters
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
-    # or
-    #valid_addresses = %w[user@example.com 
-    #                     USER@foo.COM 
-    #                     A_US-ER@foo.bar.org 
-    #                     first.last@foo.jp 
-    #                     alice+bob@baz.cn]
-    # See how much easier it is to see what is in the list when you break it apart?
+    valid_addresses = %w[user@example.com 
+                         USER@foo.COM 
+                         A_US-ER@foo.bar.org 
+                         first.last@foo.jp 
+                         alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
