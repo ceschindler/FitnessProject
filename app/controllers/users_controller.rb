605431f10d20145b8f98690f2561c3325da0590e
Derek Hobbs <dhobbs@madisoncollege.edu>
2017-04-12 17:21:16 +0000

    Added login and logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      log_in @user
+      flash[:success] = "Welcome to DC Finess Institute! Get ready to work!"
       redirect_to @user
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'static_pages/home'
 
   get 'about' => 'static_pages#about' 
@@ -10,6 +12,13 @@ Rails.application.routes.draw do
   get 'generate_meal' => 'static_pages#generateMeal'
 
   get 'user_tracking' => 'static_pages#userTracking'
+  
+  get '/login' => 'sessions#new'
+  
+  post '/login' => 'sessions#create'
+  
+  delete '/logout' => 'sessions#destroy'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
