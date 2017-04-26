37159c6d04810fc9cfce963be0df545bc273815d
Collin Schinder <cschindler@madisoncollege.edu>
2017-04-26 19:27:03 +0000

    Very broken piece of code, must redo to figure out what happened during the mailer process

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => ENV['smtp_server'],
   :port                 => ENV['smtp_port'],
-  :user_name            => ENV['smtp_user'],
+  :user_name            => ENV['smtp_user_name'],
   :password             => ENV['smtp_password'],
   :domain               => ENV['smtp_domain'],
   :authentication       => ENV['smtp_authentication']
