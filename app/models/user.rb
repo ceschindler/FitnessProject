b881c6786f03b5ade31072720b32b1811189eff1
Collin Schinder <cschindler@madisoncollege.edu>
2017-05-01 18:06:18 +0000

    Added files from account activation to make this work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token, :reset_token
+  attr_accessor :remember_token, :activation_token
   before_create :create_activation_digest
   before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
@@ -21,37 +21,7 @@ class User < ApplicationRecord
       SecureRandom.urlsafe_base64
     end
   end
-  
-  def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
-  end
-  
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
-  end
-  
-  # Sets the password reset attributes.
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
-  end
     
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
-  end
-  
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
-  
-  def forget
-    update_attribute(:remember_digest, nil)
-  end
-  
- 
-  
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
@@ -67,10 +37,11 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
   
- private
+  private
   
   def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
+  
 end
