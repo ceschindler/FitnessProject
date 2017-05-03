2fcef14ee8802c8b05a97ba26ecbdb275fb11ed2
Collin Schinder <cschindler@madisoncollege.edu>
2017-05-03 16:44:09 +0000

    Cleaned up the mailer tests on cleanup branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if @user.save && UserMailer.account_activation(@user).deliver_now
-      flash[:danger] = "Please check your email to activate your account"
+    if @user.save
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account"
       redirect_to root_url
     else
       render 'new'
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,12 +1,17 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+  
+  def setup
+    @user = users(:sarah)
+  end
+  
   test "account_activation" do
-    mail = UserMailer.account_activation
+    mail = UserMailer.account_activation(@user)
     assert_equal "Account activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    assert_equal ["sarah@example.com"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match /activation/, mail.body.encoded
   end
 
   test "password_reset" do
