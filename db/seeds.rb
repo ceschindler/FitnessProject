91fdc3600d41ab3d0541759ef16f0aaa0a167293
Derek Hobbs <dhobbs@madisoncollege.edu>
2017-05-10 16:46:36 +0000

    Fixed some indentation issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # User.create!(name:  "Example User",
-#              email: "exampl@railstutorial.org",
-#              password:              "foobar",
-#              password_confirmation: "foobar",
-#              activated: true,
-#              activated_at: Time.zone.now)
+#             email: "exampl@railstutorial.org",
+#             password:              "foobar",
+#             password_confirmation: "foobar",
+#             activated: true,
+#             activated_at: Time.zone.now)
 
 # 99.times do |n|
 #   name  = Faker::Name.name
@@ -21,32 +21,32 @@ weight = ["Dumbell-Flys", "Cable-Flys", "BenchPress", "Curls", "Squats", "Declin
 
 for i in weight
 
-    Exercise.create!(name: i,
-                     category: "WeightLifting",
-                     intensity_level: 1 + rand(2),
-                     time: 3 + rand(5),
-                     link_to_exercise: "https://www.youtube.com/user/MDUSAweightlifting")
+  Exercise.create!(name: i,
+                  category: "WeightLifting",
+                  intensity_level: 1 + rand(2),
+                  time: 3 + rand(5),
+                  link_to_exercise: "https://www.youtube.com/user/MDUSAweightlifting")
 end
 
 cardio = ["Running", "Jumping-Jacks", "Sprints", "Hill-Sprints", "Burpees"]
 
 for i in cardio
 
-    Exercise.create!(name: i,
-                     category: "Cardio",
-                     intensity_level: 1 + rand(2),
-                     time: 20 + rand(10),
-                     link_to_exercise: "https://www.youtube.com/watch?v=NKtDEgKKtwc")
+  Exercise.create!(name: i,
+                  category: "Cardio",
+                  intensity_level: 1 + rand(2),
+                  time: 20 + rand(10),
+                  link_to_exercise: "https://www.youtube.com/watch?v=NKtDEgKKtwc")
 end
 
 yoga = ["Cat", "Down-Dog", "Rag-Doll", "Tree", "BackBend", "Balancing-Stick", "Lotus", "Sage-Pose", "Bow"]
 
 for i in yoga
 
-    Exercise.create!(name: i,
-                     category: "Yoga",
-                     intensity_level: 1 + rand(2),
-                     time: 20 + rand(10),
-                     link_to_exercise: "https://www.youtube.com/channel/UCFKE7WVJfvaHW5q283SxchA")
+  Exercise.create!(name: i,
+                  category: "Yoga",
+                  intensity_level: 1 + rand(2),
+                  time: 20 + rand(10),
+                  link_to_exercise: "https://www.youtube.com/channel/UCFKE7WVJfvaHW5q283SxchA")
 end
 
