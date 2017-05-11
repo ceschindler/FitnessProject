a28fac06ef6f3446b4467343dc696153187a548d
Collin Schinder <cschindler@madisoncollege.edu>
2017-05-11 08:03:08 +0000

    attempting to configure for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "fitness_project_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => ENV['application_host'], protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
